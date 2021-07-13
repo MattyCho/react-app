3db6712adfdf31bd9718f14ae50b7224260b9028
MattyCho <myc.chosen@gmail.com>
2021-07-12 18:23:22 -0700

    added some react stuff

---
+++ src/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <h4>Author: Matt C.</h4>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
